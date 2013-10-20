78654bd7cd1dcba69b3e95f4da37186cd8508309
tayy5680 <tayy5680@gmail.com>
2013-10-20 13:31:31 +0800

    fix #13

--- main.js
+++ main.js
@@ -1,90 +1,69 @@
 /*jslint browser: true, devel: true, closure: true */
 
 var gameModule = (function () {
-	"use strict";
 
-	var timeoutVar,
-		counter = 0,
-		ballX,
-		ballY,
-		ballR,
-		scores,
-		colors = ['#8b008b', '#00008b', '#f0f8ff'],
-		length = colors.length;
+    "use strict";
 
-	function gameOver() {
-		console.log("Final:" + scores);
-	}
+    var counter = 0,
+        ballX,
+        ballY,
+        ballR,
+        scores,
+        colors = ['#8b008b', '#00008b', '#f0f8ff'],
+        length = colors.length;
 
+    function gameOver() {
+        console.log("Final:" + scores);
+    }
 
+    function startGame() {
+        var canvas = document.getElementById('game'),
+            ctx = canvas.getContext('2d');
 
-function startGame() {
-		var canvas = document.getElementById('game');
-		var ctx = canvas.getContext('2d');
+        ballX = Math.floor(Math.random() * 600);//0..600
+        ballY = Math.floor(Math.random() * 450);
+        ballR = Math.floor(Math.random() * 80);
 
-		ballX = Math.floor(Math.random() * 600)//0..600
-		ballY = Math.floor(Math.random() * 450)
-		ballR = Math.floor(Math.random() * 80)
+        canvas.width = 640;
+        canvas.height = 480;
 
-		canvas.width = 640;
-		canvas.height = 480;
+        ctx.fillStyle = colors[counter % length];
+        ctx.beginPath();
+        ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
+        ctx.fill();
 
-		ctx.fillStyle = colors[counter % length];
-		ctx.beginPath();
-		ctx.arc(ballX,ballY,ballR, 0, Math.PI * 2 , true);
-		ctx.fill();
+        if (counter >= 10000) {
+            gameOver();
+        } else {
+            setTimeout(startGame, 2000);
+            counter = counter + 1;
+        }
+    }
 
-		if (counter >= 10000) {
-			gameOver();
-		} else{
-			setTimeout(start,2000);
-			counter = counter + 1;
+    function touchEvent(evt) {
+        var x = evt.clientX,
+            y = evt.clientY,
+            tmp = (ballX - x) * (ballX - x) + (ballY - y) * (ballY - y);
 
-			
-		}
-	}
-	
+        console.log("Clicked: " + x + " , " + y);
 
+        if (tmp < ballR * ballR) {
+            scores = scores + (100 - ballR);
+            console.log("Goodjob! : "  + scores);
+        }
+    }
 
+    function start() {
+        scores = 0;
 
+        document.getElementById("main").addEventListener("click", touchEvent, false);
+        startGame();
+    }
 
+    return {
+        start : start
+    };
 
+}(document));
 
-
-
-
-
-function touchEvent(evt) {
-	var x = evt.clientX,
-		y = evt.clientY,
-		tmp = (ballX - x) * (ballX - x) + (ballY - y) * (ballY - y);
-
-	console.log("Clicked: " + x + " , " + y);
-
-	if (tmp < ballR*ballR) {
-
-		scores = scores + (100 - ballR);
-		console.log("Goodjob! : "  + scores);
-	}
-}
-
-
-
-	function start() {
-		scores = 0
-
-		document.getElementById("main").addEventListener("click", touchEvent, false);
-		startGame();
-	}
-
-	
-	return{
-		start:start
-	};
-
-
-
-
-
-} (document)) ;
 gameModule.start();
