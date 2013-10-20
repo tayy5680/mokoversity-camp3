6c24a8ae12624d67d06aec48a4943be7cffb8ac1
tayy5680 <tayy5680@gmail.com>
2013-10-20 14:56:01 +0800

    fix #14

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET users listing.
+ */
+
+varscores = [];
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.save = function(req, res){
+	scores.push(req.query.scores);
+
+	console.log("Your scores : " + scores);
+};
